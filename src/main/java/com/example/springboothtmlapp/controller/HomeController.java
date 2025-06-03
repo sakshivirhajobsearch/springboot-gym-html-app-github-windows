b8f6c3d166a84a3df275350527110cf68b9d5591
sakshivirhajobsearch <164556079+sakshivirhajobsearch@users.noreply.github.com>
2025-06-03 22:55:49 +0530

    Update HomeController.java

--- src/main/java/com/example/springboothtmlapp/controller/HomeController.java
+++ src/main/java/com/example/springboothtmlapp/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String sayHello() {
 		return "hello";
 	}
