4fe021864ea65bd305d85b63cb4d48ba084ad24a
sakshivirhajobsearch <164556079+sakshivirhajobsearch@users.noreply.github.com>
2025-06-05 08:06:41 +0530

    Update HomeController.java

--- src/main/java/com/example/springboothtmlapp/controller/HomeController.java
+++ src/main/java/com/example/springboothtmlapp/controller/HomeController.java
@@ -3,11 +3,19 @@ package com.example.springboothtmlapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+/*@Controller
 public class HomeController {
 
   @GetMapping("/home")
   public String home() {
     return "home";  // returns home.html template from /templates
   }
+}*/
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello from Spring Boot";
+    }
 }
